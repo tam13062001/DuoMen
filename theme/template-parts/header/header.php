ad2026c64270a975c1206083c6bf0772fbf841e3
tam13062001 <dtam768@gmail.com>
2025-11-10 13:31:48 +0700

    commit

--- theme/template-parts/header/header.php
+++ theme/template-parts/header/header.php
@@ -1,4 +1,4 @@
-<header class="w-full bg-gradient-to-r from-[#85CEF2] via-[#F5F5F5] to-[#85CEF2] shadow-sm ">
+<header class="w-full bg-gradient-to-r from-[#bae3f8] via-[#F5F5F5] to-[#bae3f8] shadow-sm ">
 <div class="flex items-center justify-between py-8 px-6 relative">
 
   <!-- LOGO/GRAPHIC overlay -->
@@ -10,7 +10,21 @@
 
   <!-- MENU -->
  <nav class="flex gap-10 text-[18px] font-bold text-gray-700 ml-auto space-x-6 mr-10">
-  <a href="#" class="hover:text-[#2E3690]">TRANG CHỦ</a>
+    <div class="relative inline-block">
+        <a href="#" class="hover:text-[#2E3690] font-bold text-[#2E3690] ">
+            TRANG CHỦ
+        </a>
+
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Vector_1.png' ?>" 
+            alt="" 
+            class="absolute absolute top-[-18px] right-[-14px] w-[16px]">
+
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Vector_2.png' ?>" 
+            alt="" 
+            class="absolute bottom-[-12px] right-[-10px] w-[12px]">
+    </div>
+
+  
 
   <a href="#" class="hover:text-[#2E3690]">
     Duo<span class="font-normal">men</span> BỤNG YÊN RUỘT ỔN
--- theme/templates/login.php
+++ theme/templates/login.php
@@ -3,14 +3,42 @@
 <?php get_header(); ?>
 
 
-<div class="bg-gradient-to-r from-[#85CEF2] via-[#F5F5F5] to-[#85CEF2]">
+<div class="bg-gradient-to-r from-[#bae3f8] via-[#F5F5F5] to-[#bae3f8]">
     
-    <div class="container text-center py-[100px] mx-auto">
+    <div class="container text-center py-[100px] mx-auto relative">
+
+            <!-- Nút thu gọn -->
+            <button onclick="toggleButtons()" 
+                id="btnToggle" 
+                class="fixed top-[400px] right-0 transform -translate-y-1/2  text-white p-3 bg-[#E41E25] rounded-l-full z-50 flex flex-col space-y-3">
+                <i data-lucide="video" class="w-8 h-8 bg-[#E41E25]"></i>
+            </button>
+
+            <button onclick="toggleButtons()" 
+                id="btnToggle" 
+                class="fixed top-[460px]  right-0 transform -translate-y-1/2  text-white p-3 bg-[#E41E25] rounded-l-full z-50 flex flex-col space-y-3">
+                <i data-lucide="phone-call" class="w-8 h-8 bg-[#E41E25]"></i>
+
+                
+            </button>
+
+            <button onclick="toggleButtons()" 
+                id="btnToggle" 
+                class="fixed top-[520px]  right-0 transform -translate-y-1/2  text-white p-3 bg-[#E41E25] rounded-l-full z-50 flex flex-col space-y-3">
+                <i data-lucide="message-circle" class="w-8 h-8 bg-[#E41E25]"></i>               
+            </button>
+
+            <script>
+            lucide.createIcons();
+            </script>
+
+
         <!-- HÌNH -->
         <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Frame 3.png' ?>" 
              alt="" 
              class="mx-auto mb-8 ">
-
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Vector_4.png' ?>" alt="" class="absolute top-[200px] right-[-160px]">
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Vector_3.png' ?>" alt="" class="absolute bottom-[300px] left-[-160px]">
         <!-- TEXT -->
         <div class="lg:w-[540px] text-center  mx-auto text-[#333] leading-[1.7] space-y-6">
 
@@ -27,7 +55,6 @@
     </div>
 
     <div class="relative w-full max-w-[1200px] mx-auto py-20">
-
 
         <!-- Hình nền lớn -->
         <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Group 25.png' ?>" 
@@ -139,36 +166,36 @@
 
 
     </div>
-        <div class="bg-[#30B4F6] h-[400px] relative w-full overflow-hidden">
-    <div class="max-w-[1200px] mx-auto px-6 flex  items-start">
+    <div class="bg-[#30B4F6] h-[400px] relative w-full overflow-hidden">
+        <div class="max-w-[1200px] mx-auto px-6 flex  items-start">
 
-        <!-- LEFT CONTENT -->
-        <div class="text-white pt-[80px] ">
-            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/image 10.png' ?>" 
-                 alt="MHD Logo"
-                 class="w-[120px] mb-[50px]">
+            <!-- LEFT CONTENT -->
+            <div class="text-white pt-[80px] ">
+                <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/image 10.png' ?>" 
+                    alt="MHD Logo"
+                    class="w-[120px] mb-[50px]">
 
-            <p class="text-[14px] leading-[1.6] max-w-[480px]">
-                CÔNG TY CỔ PHẦN MHD PHARMA<br>
-                Giấy chứng nhận ĐKKD số 0311203203 do Phòng Đăng Ký Kinh Doanh<br>
-                – Sở Kế hoạch Đầu tư TP. Hồ Chí Minh cấp ngày 03/10/2011
-            </p>
+                <p class="text-[14px] leading-[1.6] max-w-[480px]">
+                    CÔNG TY CỔ PHẦN MHD PHARMA<br>
+                    Giấy chứng nhận ĐKKD số 0311203203 do Phòng Đăng Ký Kinh Doanh<br>
+                    – Sở Kế hoạch Đầu tư TP. Hồ Chí Minh cấp ngày 03/10/2011
+                </p>
 
-            <p class="text-[14px] mt-4 opacity-90">
-                Bản Quyền © 2025 MHDPharma
-            </p>
+                <p class="text-[14px] mt-4 opacity-90">
+                    Bản Quyền © 2025 MHDPharma
+                </p>
+            </div>
+
+            <!-- RIGHT CONTENT -->
+            <div class="flex flex-col items-end justify-center mt-[200px] ml-[250px]">
+                <a href="#" 
+                class="bg-[#2E3690] text-white px-6 py-3 rounded-full text-[14px] font-semibold shadow-lg hover:opacity-90 transition">
+                    Facebook Vũ Trụ Duo Men
+                </a>
+            </div>
+
         </div>
-
-        <!-- RIGHT CONTENT -->
-        <div class="flex flex-col items-end justify-center mt-[200px] ml-[250px]">
-            <a href="#" 
-               class="bg-[#2E3690] text-white px-6 py-3 rounded-full text-[14px] font-semibold shadow-lg hover:opacity-90 transition">
-                Facebook Vũ Trụ Duo Men
-            </a>
-        </div>
-
     </div>
-        </div>
 
 </div>
 
